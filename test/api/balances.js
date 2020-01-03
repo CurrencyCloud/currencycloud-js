82bd24be3fa9f7868c724f271c2b02df2cb986bb
jonathancouchman <jonathan.couchman@currencycloud.com>
2020-01-03 15:33:07 +0000

    SDKJS-31 : Adds support for top up margin balance

--- test/api/balances.js
+++ test/api/balances.js
@@ -54,4 +54,21 @@ describe('balances', function() {
       .catch(done);
     });
   });
+
+  describe('topUpMargin', function () {
+    it('successfully tops up margin balance', function (done) {
+      currencyCloud.balances.topUpMargin({
+        currency: 'GBP',
+        amount: '450'})
+          .then(function (res) {
+            expect(res).is.not.empty;
+            expect(res).to.have.property('accountId').that.eql("6c046c51-2387-4004-8e87-4bf97102e36d");
+            expect(res).to.have.property('currency').that.eql("GBP");
+            expect(res).to.have.property('transferredAmount').that.eql("450.0");
+            expect(res).to.have.property('transferCompletedAt').that.eql("2007-11-19T08:37:48-06:00");
+            done();
+          })
+          .catch(done);
+    });
+  });
 });
